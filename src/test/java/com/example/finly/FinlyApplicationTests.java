e63b03ac2eb780d778f0e86e29ad2324dc18715e
Mysty47 <106233800+Mysty47@users.noreply.github.com>
2025-07-13 23:12:14 +0300

    Startup

---
+++ src/test/java/com/example/finly/FinlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
